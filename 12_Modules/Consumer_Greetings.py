7d9cd014c4f261802d8188d8fb352f4c6686ac88
Ramakant Debata <ramakant.s.debata@gmail.com>
2026-03-04 14:06:03 +0530

    Day 03

---
+++ 12_Modules/Consumer_Greetings.py
@@ -0,0 +1,9 @@
+# import greeting
+
+# greeting.greet()
+# greeting.greetName("Rakesh")
+
+from greeting import greet, greetName
+
+greet()
+greetName("Vijay")
---
+++ 12_Modules/greeting.py
@@ -0,0 +1,26 @@
+def greet():
+    print("Hi there!")
+
+def greetName(name):
+    greeting = "Hello"
+    final_greeting = PrepGreeting(greeting, name)
+    print(final_greeting)
+
+def PrepGreeting(greeting, name):
+    return greeting + " " + name + "!!"
+
+def greetInteractive():
+    name = input("Enter your name:")
+    greeting = "Hey"
+    final_greeting = PrepGreeting(greeting, name)
+    print(final_greeting)
+
+def Test():
+    greet()
+    greetName("Manish")
+    greetInteractive()
+
+# print(f"{__name__ = }")
+
+if __name__ == '__main__':
+    Test()
